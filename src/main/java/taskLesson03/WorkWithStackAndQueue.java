5a15f592ac0056567017159dae467a194bda0ac0
kirill <kirill261989@mail.ru>
2021-03-19 18:24:46 +0400

    add 03 Task Lesson (test)

--- src/main/java/taskLesson03/WorkWithStackAndQueue.java
+++ src/main/java/taskLesson03/WorkWithStackAndQueue.java
@@ -1,7 +1,61 @@
 package taskLesson03;
 
+import java.util.Deque;
+
 public class WorkWithStackAndQueue {
+
+/**
+ * 1. Создать программу, которая переворачивает вводимые строки (читает справа налево).
+ */
+
+    public static String reversString (String str){
+        if(str.equals("") || str.equals(" ")){
+            throw new RuntimeException("В введённой строке отсутствуют символы");
+        }
+
+        MyStack<Character> myStack = new MyStack<>(str.length());
+        for(int i = 0; i < str.length(); i++){
+            myStack.push(str.charAt(i));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(myStack.size() != 0){
+            sb.append(myStack.pop());
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args){
+//        String str = "Я знаю, что ничего не знаю.";
+//        System.out.println(str);
+//        System.out.println(reversString(str));
+
+        MyDeque<Integer> md = new MyDeque<>(4);
+
+//        md.insertLeft(100);
+//        md.insertLeft(200);
+//        md.insertLeft(10);
+//        md.insertLeft(20);
+
+//        md.insertRight(100);
+//        md.insertRight(200);
+//        md.insertRight(10);
+//        md.insertRight(20);
+
+        md.insertRight(100);
+        md.insertRight(200);
+        md.insertLeft(10);
+        md.insertLeft(20);
+
+
+        StringBuilder sb = new StringBuilder();
+        while(md.size() != 0){
+            sb.append(md.removeLeft()).append(" ");
+//            sb.append(md.removeRight()).append(" ");
+        }
+
+
+        System.out.println(sb.toString());
 
     }
 
